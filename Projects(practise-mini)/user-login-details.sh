fcd3b1e521f611405d76003c5df22e09da574b45
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-11 23:25:02 +0530

    Updated to the previous one

--- Projects(practise-mini)/user-login-details.sh
+++ Projects(practise-mini)/user-login-details.sh
@@ -1,14 +1,38 @@
+#code for displaying specified user details
+
 echo "Enter a username: \c"
 
-func()
+#Asking for the username and checking weather the name is exists or not
+uname_pass()
 {
 read uname
 grep $uname /etc/passwd >> /dev/null
+a=$?
 }
 
-funt()
+uname_pass
+
+#Main execution.
+login_dtls(){
+	echo "Checking.\c"
+        sleep 1s
+	a=2
+        while [ $a -gt 0 ] #little time delay for checking dot(.)
+        do
+               echo " .\c"
+               a=`expr $a - 1`
+               sleep 1s
+        done
+	set `who | grep $uname`
+	echo "\nLogin user name is = $1" ; sleep 1s
+	echo "Login date is $3 at $4 hr's." 
+	exit
+}
+
+#Asking weather to check with othernames, if the previous one fails.
+rntr_uname()
 {
-echo "Incorrect username\nDo you want to try again[Y/N]: \c"
+echo "Incorrect username. Do you want to try again[Y/N]: \c"
 read ans
 	if [ $ans = "Y" -o $ans = "y" ]
 	then
@@ -18,31 +42,19 @@ read ans
 		echo "Abort"
 		exit
 	fi
-func
 }
 
-func
-
-if [ $? -eq 0 ]
+if [ $a -eq 0 ]
 then
-	continue
+	login_dtls
 else
-	funt
+        until [ $a -eq 0 ]
+	do 
+		rntr_uname
+		uname_pass
+		if [ $a -eq 0 ]
+		then
+			login_dtls
+		fi
+	done
 fi
-
-while [ $? -eq 0 ]
-do
-	echo "Checking\c"
-         sleep 1s
-         a=3
-         while [ $a -gt 0 ]
-         do
-                echo " .\c"
-                a=`expr $a - 1`
-                sleep 1s
-         done
-	set `who | grep $uname`
-	echo "\nLogin user name is = $1" ; sleep 1s
-	echo "Login date is $3 at $4 hr's." 
-	exit
-done
