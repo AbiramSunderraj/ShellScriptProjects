571c6850214ae4b58d72011f149b79d82d691c8d
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-24 23:04:45 +0530

    Added additional search functionality

--- Projects(practise-mini)/Addressbook-Management/Main.sh
+++ Projects(practise-mini)/Addressbook-Management/Main.sh
@@ -1,5 +1,29 @@
 #addressbook management with search, edit, manipulate and preview features.
 
+IFS=":"
+
+main_option(){          #Options
+echo "Select an option to continue.\nOption '1' for new entry.\nOption '2' search in the addressbook.\nOption '3' for edit an entry. \n\nEnter an option [1/2/3]: \c"
+read opt
+case $opt in
+1)
+        echo "Add new Entry"
+        new_entry
+        input_conformation
+        ;;
+2)
+        search
+        ;;
+3)
+        echo "Enter username to edit: \c"
+        read unamee
+        ;;
+*)
+        echo "Please try again."
+        ;;
+esac
+}
+
 new_entry()		#For new user, creates a new row for a user.
 {
 echo "Enter Username, Mobile number & E-mail id:"
@@ -19,7 +43,7 @@ echo "Does the Entered information is correct? Select Y|y to continue or anyi ot
 read conf
 if [ "$conf" = "Y" -o "$conf" = "y" ]
 then
-        echo "$fname\t$lname\t$mnum\t$mid" >> userdetails
+        echo "$fname:$lname:$mnum:$mid" >> userdetails
 else
         new_entry
 fi
@@ -28,7 +52,9 @@ fi
 search(){		#Searching the user details in the file
 echo "Enter name or Phone number or E-mail id of the user or even a single phrase: \c"
 read search_item
-grep -i $search_item userdetails
+column -s":" -t userdetails | grep -i "$search_item"
+echo "\n\n"
+main_option
 }
 
 modify_entry()		#To modify the previously entered informations
@@ -57,27 +83,12 @@ D)
 esac
 }
 
-main_option(){		#Options
-read opt
-case $opt in
-1)
-	echo "Add new Entry"
-	new_entry
-	input_conformation
-	;;
-2)	
-	search
-	;;
-3)	
-	echo "Enter username to edit: \c"
-	read unamee
-	;;
-*)
-	echo "Please try again."
-	;;
-esac
+delete_entry()
+{
+echo "Enter"
+
 }
 
-echo "Hai welcome to the Addressbook, here you can able to add, search and update the details of user. Select an option to continue.\nOption '1' for new entry.\nOption '2' search in the addressbook.\nOption '3' for edit an entry.\n\nEnter an option [1/2/3]: \c"
+echo "Hai welcome to the Addressbook, here you can able to add, search and update the details of user."
 
 main_option
