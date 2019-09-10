216af7120025243388481e16f40974096b1b0250
Abiram Sundarraj <abiramsundar@gmail.com>
2019-09-10 21:29:22 +0530

    Add files via upload

--- Automation Process/Backup/backup.sh
+++ Automation Process/Backup/backup.sh
@@ -1,5 +1,8 @@
-#Backup automation - 08/09/2019up
+#Backup automation - 08/09/2019
 #set -x
+echo -e "\n*************** BACKUP PROCESS ****************"
+echo -e "*FOR BETTER EXPIRENCE RUN WITH ROOT PRIVILAGES*" #Next implementation: run if the user is root or exit
+
 source ./function.sh
 
 type_select
--- Automation Process/Backup/function.sh
+++ Automation Process/Backup/function.sh
@@ -1,5 +1,50 @@
+backup_loc(){
+	echo -e "\nSelect storage type.\nA. Local Storage(Device attached to this PC)\nB. Cloud Storage(To another server/storage devices.)"
+	echo -e "\nOption[A/B]: \c"
+	read strg_type
+	case $strg_type in
+		'A' | 'a' )
+			echo -e "\nEnter device location: \c"
+			read dest_loc
+			#echo "Source Location is $src_loc"
+			echo -e "\nSelected storage type is 'Local Storage'. Device location is '$dest_loc'.Type 'YES' to continue: \c"
+			read ans
+			case $ans in
+				'YES' | 'Yes' | 'yes' )
+					echo "Backup Starting. . ."
+					rsync -azh $src_loc/ $dest_loc
+					#archive and extraction concept
+					echo "Backup Finished. . ."
+					;;
+				* )
+					backup_loc
+					;;
+			esac
+			#some command - while writing - rsync and archive | next time - archive and rsync.(rsync && tar)
+			;;
+		'B' | 'b' )
+			echo -e "\nEnter device location: \c"
+			read dev_loc
+			echo -e "\nSelected storage type is 'Cloud Storage'\nDevice location is '$dev_loc'. Type 'YES' to continue: \c"
+			;;
+		*)
+			echo "\nSelect a valid option."
+			;;
+		esac
+}
+
+
+
+
+
+
+
+
+
+
+
 type_select(){
-	echo -e "Select the type of backup\nA. Full Backup\nB. Incremental Backup\nC. Partial Backup\n\nOption[A/B/C]:\c"
+	echo -e "\nSelect the type of backup\nA. Full Backup\nB. Incremental Backup\nC. Partial Backup\n\nOption[A/B/C]:\c"
 	read opt
         case $opt in
                 'A' | 'B' | 'C' | 'a' | 'b' | 'c' )
@@ -17,24 +62,31 @@ type_select(){
                         fi
                         ;;
                 *)
-                        echo -e "Incorrect Option. Select a correct option [A/B/C]: \c"
+                        echo -e "\nIncorrect Option. Select a correct backup type."
 			type_select
         esac
 }
 
 location_select(){
-	echo -e "\nSpecify a path to backup: \c"
-	read loc
-	echo -e "\nSpecfied path is: '$loc'\nType 'YES' to continue: \c"
+	echo -e "\nSpecify path to backup: \c"
+	read src_loc
+	ls $src_loc >>/dev/null 2>&1
+	if [ "$?" -eq 0 ]
+	then
+	echo -e "\nSpecfied path is: '$src_loc'\nType 'YES' to continue or press any key to re-specify the location again: \c"
 	read loc_opt
 	case $loc_opt in
 		'YES' | 'Yes' | 'yes' )
 			echo -e "Backup process continuing. . ."
+			backup_loc
 			;;
 		* )
-			echo "Incorrect Option. Enter the locatiot
-			n once again."
+			echo -e "\nSpecify the location again."	
 			location_select
 			;;
-	esac	
+	esac
+	else
+		echo -e "\nPath does not exist or Root privilages required. Specify the correct path or run with root privilages."
+		location_select
+	fi
 }
