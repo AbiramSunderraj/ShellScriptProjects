93e4a9656df44d2ac50d18ad5601f0f85bc6eb75
Abiram Sundarraj <abiramsundar@gmail.com>
2019-09-11 20:09:19 +0530

    Add files via upload

--- Automation Process/Backup/function.sh
+++ Automation Process/Backup/function.sh
@@ -1,4 +1,72 @@
-backup_loc(){
+type_select(){ # Select the type of backup. Full, Incremental and Partial
+        echo -e "\nSelect the type of backup\nA. Full Backup\nB. Incremental Backup\nC. Partial Backup\n\nOption[A/B/C]:\c"
+        read bkup_opt
+        case $bkup_opt in
+                'A' | 'B' | 'C' | 'a' | 'b' | 'c' )
+                        if [ "$bkup_opt" = 'A' -o "$bkup_opt" = 'a' ]
+                        then
+                                echo -e "\nFull Backup Selected. . ."
+                                opt=""
+                        elif [ "$bkup_opt" = 'B' -o "$bkup_opt" = 'b' ]
+                        then
+                                echo -e "\nIncremental Backup Selected. . ."
+                                opt=""
+                        else [ "$bkup_opt" = 'C' -o "$bkup_opt" = 'c' ]
+                                echo -e "\nPartial Backup Selected. . ."
+                                opt=""
+                        fi
+                        ;;
+                *)
+                        echo -e "\nIncorrect Option. Select a correct backup type."
+                        type_select
+        esac
+}
+
+################## Actual backup functions ####################
+
+lfbkup(){ #Local Full backup, Just copying everything to local drive.
+	echo -e "Backup in progress. . ."
+        cp -rfp $src_loc/ $dest_loc
+        #archive and extraction concept
+	size=`du -sh $dest_loc`
+	set "$size"
+        echo -e "Backup process completed. . .\nBackedup $1 files to the destination folder."
+}
+
+libkup(){ #Local Incremental backup, Sync'ing the files in the folders  using rsync
+        echo -e "Backup in progress. . ."
+        #rsync
+        echo -e "Backup process completed. . .\n"
+}
+
+lpbkup(){ #Local Partial backup, Selected type of contents and as by date last accessed or modified using amc-time
+        echo -e "Backup in progress. . ."
+        #only the type of contents or date modified.
+        echo -e "Backup process completed. . .\n"
+}
+
+
+cfbkup(){ #Cloud Full backup, Just copying everything to local drive.
+        echo -e "Backup in progress. . ."
+        cp -rfp $src_loc/ $dest_loc
+        #archive and extraction concept
+        echo -e "Backup process completed. . .\n"
+}
+
+cibkup(){ #Cloud Incremental backup, Sync'ing the files in the folders using rsync
+        echo -e "Backup in progress. . ."
+        #rsync
+        echo -e "Backup process completed. . .\n"
+}
+
+cpbkup(){ #Cloud Partial backup, Selected type of contents and as by date last accessed or modified using amc-time
+        echo -e "Backup in progress. . ."
+        #only the type of contents or date modified.
+        echo -e "Backup process completed. . .\n"
+}
+
+
+backup_loc(){  ##Backup location selection, Local or cloud.
 	echo -e "\nSelect storage type.\nA. Local Storage(Device attached to this PC)\nB. Cloud Storage(To another server/storage devices.)"
 	echo -e "\nOption[A/B]: \c"
 	read strg_type
@@ -6,69 +74,57 @@ backup_loc(){
 		'A' | 'a' )
 			echo -e "\nEnter device location: \c"
 			read dest_loc
-			#echo "Source Location is $src_loc"
-			echo -e "\nSelected storage type is 'Local Storage'. Device location is '$dest_loc'.Type 'YES' to continue: \c"
+			echo -e "\nSelected storage type is 'Local Storage'. Device location is '$dest_loc'.\nThis method overwrites the previous duplicate files. Type 'YES' to continue: \c"
 			read ans
 			case $ans in
 				'YES' | 'Yes' | 'yes' )
-					echo "Backup Starting. . ."
-					rsync -azh $src_loc/ $dest_loc
-					#archive and extraction concept
-					echo "Backup Finished. . ."
+					if [ "$bkup_opt" = 'A' -o "$bkup_opt" = 'a' ]
+                        		then
+						lfbkup
+                        		elif [ "$bkup_opt" = 'B' -o "$bkup_opt" = 'b' ]
+                        		then
+						libkup
+                                	else [ "$bkup_opt" = 'C' -o "$bkup_opt" = 'c' ]
+						lpbkup
+                        		fi
 					;;
 				* )
+					echo -e "\nWrong Input\n"
 					backup_loc
 					;;
 			esac
-			#some command - while writing - rsync and archive | next time - archive and rsync.(rsync && tar)
 			;;
+
 		'B' | 'b' )
 			echo -e "\nEnter device location: \c"
-			read dev_loc
-			echo -e "\nSelected storage type is 'Cloud Storage'\nDevice location is '$dev_loc'. Type 'YES' to continue: \c"
-			;;
+                        read dest_loc
+                        echo -e "\nSelected storage type is 'Local Storage'. Device location is '$dest_loc'.\nThis method overwrites the previous duplicate files. Type 'YES' to continue: \c"
+                        read ans
+                        case $ans in
+                                'YES' | 'Yes' | 'yes' )
+                                        if [ "$bkup_opt" = 'A' -o "$bkup_opt" = 'a' ]
+                                        then
+                                                cfbkup
+                                        elif [ "$bkup_opt" = 'B' -o "$bkup_opt" = 'b' ]
+                                        then
+                                                cibkup
+                                        else [ "$bkup_opt" = 'C' -o "$bkup_opt" = 'c' ] 
+                                                cpbkup
+                                        fi
+                                        ;;
+                                * )
+                                        backup_loc
+                                        ;;
+                        esac
+                        ;;
 		*)
 			echo "\nSelect a valid option."
 			;;
 		esac
 }
 
-
-
-
-
-
-
-
-
-
-
-type_select(){
-	echo -e "\nSelect the type of backup\nA. Full Backup\nB. Incremental Backup\nC. Partial Backup\n\nOption[A/B/C]:\c"
-	read opt
-        case $opt in
-                'A' | 'B' | 'C' | 'a' | 'b' | 'c' )
-                        if [ "$opt" = 'A' -o "$opt" = 'a' ]
-                        then
-                                echo -e "\nFull Backup Selected, Script is now continuing. . ."
-				opt=""
-                        elif [ "$opt" = 'B' -o "$opt" = 'b' ]
-                        then
-                                echo -e "\nIncremental Backup Selected, Script is now continuing. . ."
-				opt=""
-                        else [ "$opt" = 'C' -o "$opt" = 'c' ]
-                                echo -e "\nPartial Backup Selected, Script is now continuing. . ."
-				opt=""
-                        fi
-                        ;;
-                *)
-                        echo -e "\nIncorrect Option. Select a correct backup type."
-			type_select
-        esac
-}
-
-location_select(){
-	echo -e "\nSpecify path to backup: \c"
+location_select(){ ##Source path selection to take backup.
+	echo -e "\nSpecify source path: \c"
 	read src_loc
 	ls $src_loc >>/dev/null 2>&1
 	if [ "$?" -eq 0 ]
