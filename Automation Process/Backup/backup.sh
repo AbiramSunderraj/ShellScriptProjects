250a0ed648eb4ad4f13fde354ff6807a9ba2a9df
Abiram Sundarraj <abiramsundar@gmail.com>
2019-09-12 20:34:33 +0530

    Added #root user check for execution of script

--- Automation Process/Backup/backup.sh
+++ Automation Process/Backup/backup.sh
@@ -1,9 +1,17 @@
 #Backup automation - 08/09/2019
 #set -x
-echo -e "\n*************** BACKUP PROCESS ****************"
-echo -e "*FOR BETTER EXPIRENCE RUN WITH ROOT PRIVILAGES*" #Next implementation: run if the user is root or exit
 
-source ./function.sh
+echo -e "\nWelocome to backup utility script. (v 0.1)"
 
-type_select
-location_select
+source ./function.sh #Path to the backup function's source file.
+
+user=`whoami`             #Checking the username of the current tty user.
+
+if [ "$user" = "root" ]   #run the script if it is root user, else exit.
+then			  #Provide the function names below.
+	type_select
+	location_select
+else
+	echo -e "\n##Run this script as a root user.##\n"
+	exit 0
+fi
