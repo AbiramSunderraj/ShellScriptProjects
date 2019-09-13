b06ddcd85374653e3da278c315098a9780d3ecfa
Abiram Sundarraj <abiramsundar@gmail.com>
2019-09-13 20:58:37 +0530

    Added several Features

--- Automation Process/Backup/backup.sh
+++ Automation Process/Backup/backup.sh
@@ -1,17 +1,16 @@
-#Backup automation - 08/09/2019
+#Started @ 08/09/2019 by Abiram Sundarraj
 #set -x
 
 echo -e "\nWelocome to backup utility script. (v 0.1)"
 
 source ./function.sh #Path to the backup function's source file.
 
+
 user=`whoami`             #Checking the username of the current tty user.
 
 if [ "$user" = "root" ]   #run the script if it is root user, else exit.
-then			  #Provide the function names below.
-	type_select
-	location_select
+then	pbkup		  #Provide the function names below.
+	#type_select
 else
 	echo -e "\n##Run this script as a root user.##\n"
-	exit 0
 fi
