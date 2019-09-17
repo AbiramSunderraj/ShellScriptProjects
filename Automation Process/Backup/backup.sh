07435a3cf5b176332dedb8c8f29daa374416e3bc
Abiram Sundarraj <abiramsundar@gmail.com>
2019-09-17 23:42:05 +0530

    Add files via upload

--- Automation Process/Backup/backup.sh
+++ Automation Process/Backup/backup.sh
@@ -1,7 +1,7 @@
 #Started @ 08/09/2019 by Abiram Sundarraj
 #set -x
-
-echo -e "\nWelocome to backup utility. (v 0.1)"
+clear
+echo -e "************************Backup utility main menu (v 0.1)************************"
 
 source ./function.sh #Path to the backup function's source file.
 
@@ -9,8 +9,8 @@ source ./function.sh #Path to the backup function's source file.
 user=`whoami`             #Checking the username of the current tty user.
 
 if [ "$user" = "root" ]   #run the script if it is root user, else exit.
-then	pbkup		  #Provide the function names below.
-	#type_select
+then
+str_loc  #Provide the function names below.
 else
-	echo -e "\n##Run this script as a root user.##\n"
+echo -e "\n##Run this script as a root user.##\n"
 fi
